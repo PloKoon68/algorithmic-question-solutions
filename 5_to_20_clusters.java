69d586a4ea3a1ba62e67be5cb4efba971072b55b
Mehmet <75950443+PloKoon68@users.noreply.github.com>
2022-08-09 19:40:04 +0300

    Update 5_to_20_clusters.java

--- 5_to_20_clusters.java
+++ 5_to_20_clusters.java
@@ -1,1 +1,34 @@
+/*
+If clusters among the numbers from1 to 20 are formed with digits varying between 5and 20,
+how many of these sets will have the sum of 75 for its digits?
+*/
 
+
+
+public class Main {
+
+    static int count;
+
+    public static void main(String[] args) {
+        for(int i = 5; i < 21; i++)
+            func(i, 1, 0, 1);
+
+        System.out.println(count);
+    }
+
+
+
+    public static void func(int border, int start, int sum, int dimension) {
+        for(int i = start; i < 21; i++) {
+            if(dimension != border) {
+
+                func(border, i + 1, sum + i, dimension + 1);
+
+            }
+            else
+                if(sum + i == 75)
+                    count++;
+
+        }
+    }  
+}
