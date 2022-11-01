1044c5a3e9ca63b4c6704aa03d8060b293c96f36
Mehmet <75950443+PloKoon68@users.noreply.github.com>
2022-11-01 15:32:05 +0300

    Update Where Will the Ball Fall.java

--- Where Will the Ball Fall.java
+++ Where Will the Ball Fall.java
@@ -1,3 +1,28 @@
 //leetcode question link
 //https://leetcode.com/problems/where-will-the-ball-fall/
 
+class Solution {
+    public static int[] findBall(int[][] grid) {
+        int[] keys = new int[grid[0].length];
+        
+        for(int column = 0; column < grid[0].length; column++) 
+            keys[column] = dropBall(grid, column);
+        
+        
+        return keys;
+    }
+    
+    public static int dropBall(int[][] grid, int column) {
+        int j = column;
+        for(int row = 0; row < grid.length; row++) {
+            //checl if -1
+            if((grid[row][j] == 1 && ((j == grid[0].length - 1) || grid[row][j+1] == -1)) || 
+                    (grid[row][j] == -1 && ((j == 0) || grid[row][j-1] == 1)))
+                return -1;
+            
+            j = (grid[row][j]==1)? j + 1: j-1;
+        }
+        return j;
+    }
+  
+}
