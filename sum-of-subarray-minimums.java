a6eee320cf90f7ca5a862b43e8617ced6ee9a9db
Mehmet <75950443+PloKoon68@users.noreply.github.com>
2022-11-27 16:34:39 +0300

    Update sum-of-subarray-minimums.java

--- sum-of-subarray-minimums.java
+++ sum-of-subarray-minimums.java
@@ -8,7 +8,7 @@ Since the answer may be large, return the answer modulo 109 + 7.
 public Solution {
   
   //first solution
-  public static int sumSubarrayMins1(int[] arr) {
+  public int sumSubarrayMins1(int[] arr) {
         int sum = 0;
         for(int len = 1; len <= arr.length; len++) {
             for(int start = 0; start <= arr.length - len; start++) {
@@ -22,7 +22,45 @@ public Solution {
         }
         return sum % (int)(Math.pow(10,9) + 7);
     }
+  
+  //second solution, which is more efficient and fast 
+  public static int sumSubarrayMins2(int[] arr) {
+        int sum = 0;
+        LinkedList<Integer> list = new LinkedList<>();
+        int listPoint;
 
+        for(int start = 0; start < arr.length; start++) {
+            listPoint = 0;
+            int end = arr.length - 1;
+
+            //remove redundants from list
+            int i = list.size() - 1;
+            while(i >= 0 && list.get(i) < start) {
+                list.remove(i);
+                i--;
+            }
+            while(end >= start) {
+                if(listPoint == list.size()) {
+                    int min = arr[start];
+                    int add_ind = start;
+                    for(int j = start; j <= end; j++)
+                        if(min > arr[j]) {
+                            add_ind = j;
+                            min = arr[j];
+                        }
+                    list.add(add_ind);
+                }
+                int index = list.get(listPoint);
+                listPoint++;
+                sum += (arr[index] * ((end - index) + 1));
+                end = index - 1;
+            }
+
+            sum %= (int)(Math.pow(10,9) + 7);
+        }
+
+        return sum;
+    }
 
 
 }
