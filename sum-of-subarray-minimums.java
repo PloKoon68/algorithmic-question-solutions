e947232b5ad9fee63c543bfdf6dd955e3322e2f5
Mehmet <75950443+PloKoon68@users.noreply.github.com>
2022-11-27 16:33:46 +0300

    Update sum-of-subarray-minimums.java

--- sum-of-subarray-minimums.java
+++ sum-of-subarray-minimums.java
@@ -6,4 +6,23 @@ Since the answer may be large, return the answer modulo 109 + 7.
 */
 
 public Solution {
+  
+  //first solution
+  public static int sumSubarrayMins1(int[] arr) {
+        int sum = 0;
+        for(int len = 1; len <= arr.length; len++) {
+            for(int start = 0; start <= arr.length - len; start++) {
+                int min = arr[start];
+                for(int i = start; i < start + len; i++) {
+                    if(arr[i] < min)
+                        min = arr[i];
+                }
+                 sum += min;
+            }
+        }
+        return sum % (int)(Math.pow(10,9) + 7);
+    }
+
+
+
 }
