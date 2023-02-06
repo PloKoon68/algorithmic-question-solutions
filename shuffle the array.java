7e662eafa0819781cf2c70848e73f56bd8e3bcaf
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-02-06 18:54:10 +0300

    Update shuffle the array.java

--- shuffle the array.java
+++ shuffle the array.java
@@ -1,1 +1,15 @@
+//easy
+//https://leetcode.com/problems/shuffle-the-array/description/
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] arr = new int[n * 2];
+        int i = 0, j=0;
+        while(i < arr.length - 1) {
+            arr[i++] = nums[j];
+            arr[i++] = nums[n + j++];
+            }
+            return arr;
+    }
+}
 
