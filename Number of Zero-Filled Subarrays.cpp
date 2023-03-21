3157da4226e214d1a042591b8a20edd65f347e1f
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-03-21 16:08:22 +0300

    Update Number of Zero-Filled Subarrays.cpp

--- Number of Zero-Filled Subarrays.cpp
+++ Number of Zero-Filled Subarrays.cpp
@@ -1,2 +1,28 @@
 //https://leetcode.com/problems/number-of-zero-filled-subarrays/
 
+
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+      long long sum = 0;
+        for(int i = 0; i < nums.size(); i++) {
+          if(nums[i] == 0) {
+            int j = i;
+            long count = 0;
+            while(j < nums.size()) {
+              if(nums[j] == 0) {
+                count++;
+                j++;
+              }
+              else 
+                break;
+            }
+            i = j;
+            
+            sum += count * (count + 1) / 2;
+          }
+        }
+      return sum;
+    }
+};
