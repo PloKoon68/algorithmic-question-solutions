8e134a8b98d224a1f58f05f345bbd44c2d28bebc
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-04-13 16:25:21 +0300

    Update root estimate methods.java

--- numerical analysis/root estimate methods.java
+++ numerical analysis/root estimate methods.java
@@ -13,4 +13,30 @@ public class Main {
         return Math.sin(x);
     }
   
+    public static void bisection(double a, double b, int iterate) {
+        for(int i = 0; i < iterate; ++i) {
+            double cur = (b - a) / 2.0D;
+            System.out.print(cur - a);
+            if (realFunc(cur) * realFunc(a) > 0.0D) {
+                a = cur;
+            } else {
+                b = cur;
+            }
+        }
+
+    }
+
+    public static void falsePosition(double l, double r, int iterate) {
+        for(int i = 0; i < iterate; ++i) {
+            double cur = (l * realFunc(r) - r * realFunc(l)) / (realFunc(r) - realFunc(l));
+            System.out.println("x: " + cur + "    f(x): " + realFunc(cur));
+            if (realFunc(cur) * realFunc(l) > 0.0D) {
+                l = cur;
+            } else {
+                r = cur;
+            }
+        }
+
+    }
+
 }
