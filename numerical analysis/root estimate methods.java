3657e2185303b6589bb8a0fb573ac434c18afa6d
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-04-13 16:25:02 +0300

    Update root estimate methods.java

--- numerical analysis/root estimate methods.java
+++ numerical analysis/root estimate methods.java
@@ -5,5 +5,12 @@ import java.util.List;
 
 
 public class Main {
+    public static double realFunc(double x) {
+        return x * Math.exp(0.5D * x) + 1.2D * x - 5.0D;
+    }
+
+    public static double derrivative(double x) {
+        return Math.sin(x);
+    }
   
 }
