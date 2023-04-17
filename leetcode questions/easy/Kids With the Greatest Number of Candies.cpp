a2a8719a8d13b71cad310f453608809b4b1ca216
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-04-17 13:31:41 +0300

    Update Kids With the Greatest Number of Candies.cpp

--- leetcode questions/easy/Kids With the Greatest Number of Candies.cpp
+++ leetcode questions/easy/Kids With the Greatest Number of Candies.cpp
@@ -1,5 +1,18 @@
 //https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
 
 
+class Solution {
+public:
+    
+  vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+    vector<bool> result(candies.size());
+    int max = -1;
+    for(int i: candies)
+      if(max < i)
+        max = i;
 
-
+    for(int i = 0;i < candies.size(); i++)
+        result[i] = (candies[i] + extraCandies >= max);
+    return result;
+  }
+};
