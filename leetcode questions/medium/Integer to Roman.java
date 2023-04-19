fd771e66cf338ac60fe234438b9925eafcccb0d9
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-04-19 17:31:58 +0300

    Create Integer to Roman.java

---
+++ leetcode questions/medium/Integer to Roman.java
@@ -0,0 +1,38 @@
+//converting integers from 1 to 3999 to roman number.
+//https://leetcode.com/problems/integer-to-roman/description/
+
+class Solution {
+     public String intToRoman(int num) {
+        char[] priority = {'I','V','X', 'L', 'C','D', 'M'};
+        StringBuffer roman = new StringBuffer();
+
+        int i = 0;
+        while(num > 0) {
+            int mod = num % 10;
+
+            if(mod <= 3)
+                for(int j = 0; j < mod; j++)
+                    roman.insert(0, priority[i]);
+            else if(mod <= 8) {
+                if(mod == 4) {
+                    roman.insert(0, priority[i+1]);
+                    roman.insert(0, priority[i]);
+                }
+                else {
+                    for(int j = 0; j < mod - 5; j++)
+                        roman.insert(0, priority[i]);
+                    roman.insert(0, priority[i + 1]);
+                }
+            }
+            else {
+                roman.insert(0, priority[i + 2]);
+                roman.insert(0, priority[i]);
+            }
+
+            num /= 10;
+            i += 2;
+        }
+
+        return  roman.toString();
+    }
+}
