dcf043115114e059b5223c2c8d97063820d7b251
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-04-20 18:48:08 +0300

    Update and rename leetcode questions/Employees Earning More Than Their Managers.sql to leetcode questions/SQL/easy/Employees Earning More Than Their Managers.sql

--- leetcode questions/SQL/easy/Employees Earning More Than Their Managers.sql
+++ leetcode questions/SQL/easy/Employees Earning More Than Their Managers.sql
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/employees-earning-more-than-their-managers/
+
 # Write your MySQL query statement below
 SELECT e.name AS Employee FROM Employee e, Employee m
 WHERE m.id = e.managerId AND e.salary > m.salary;
