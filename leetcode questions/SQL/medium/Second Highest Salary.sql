c7ba4a5571e5c48947670e18ebc1fec60f7c9e2f
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-04-27 01:09:47 +0300

    Update Second Highest Salary.sql

--- leetcode questions/SQL/medium/Second Highest Salary.sql
+++ leetcode questions/SQL/medium/Second Highest Salary.sql
@@ -5,3 +5,6 @@ SELECT COALESCE((SELECT s FROM (SELECT salary AS s, SUM(id) FROM Employee
 GROUP BY salary
 ORDER BY salary DESC
 LIMIT 1,1) AS t), NULL) AS SecondHighestSalary;
+
+
+/*An easier solution I found later on: Select max(salary) AS SecondHighestSalary from employee where salary not in (select max(salary) from employee)*/
