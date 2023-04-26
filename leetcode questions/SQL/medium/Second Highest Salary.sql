32839dfb2135c7b56dd2ffbbded074b458be1549
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-04-26 22:38:56 +0300

    Update Second Highest Salary.sql

--- leetcode questions/SQL/medium/Second Highest Salary.sql
+++ leetcode questions/SQL/medium/Second Highest Salary.sql
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/second-highest-salary/
+
 # Write your MySQL query statement below
 SELECT COALESCE((SELECT s FROM (SELECT salary AS s, SUM(id) FROM Employee
 GROUP BY salary
