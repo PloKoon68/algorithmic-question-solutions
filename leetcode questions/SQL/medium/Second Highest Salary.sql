71807bf3b768261cc6c3c9de4a0475fa06e75260
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-04-26 22:38:38 +0300

    Create Second Highest Salary.sql

---
+++ leetcode questions/SQL/medium/Second Highest Salary.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT COALESCE((SELECT s FROM (SELECT salary AS s, SUM(id) FROM Employee
+GROUP BY salary
+ORDER BY salary DESC
+LIMIT 1,1) AS t), NULL) AS SecondHighestSalary;
