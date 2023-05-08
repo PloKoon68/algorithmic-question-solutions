9deb59a8dbacde180685eb688443f2a4fba4b12f
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-05-08 11:54:28 +0300

    Update Matrix Diagonal Sum.cpp

--- leetcode questions/ds-algorithm/easy/Matrix Diagonal Sum.cpp
+++ leetcode questions/ds-algorithm/easy/Matrix Diagonal Sum.cpp
@@ -1,1 +1,21 @@
 //https://leetcode.com/problems/matrix-diagonal-sum/description/
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int i = 0;
+        int sum = 0, size = mat.size();
+
+        //with odd number element matrix, add the middle element manually
+        sum += (size%2==1)? mat[size/2][size/2]: 0;
+      
+        while(i < size/2) {
+          sum += mat[i][i];
+          sum += mat[i][(size-1)-i];
+          sum += mat[(size-1)-i][(size-1)-i];
+          sum += mat[(size-1)-i][i];
+          i++;
+        }
+      return sum;
+    }
+};
