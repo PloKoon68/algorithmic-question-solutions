8a5380c14f06079e501e50ad3891e5e54e3b74d1
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-05-15 14:57:59 +0300

    Update Swapping Nodes in a Linked List.cpp

--- leetcode questions/ds-algorithm/medium/Swapping Nodes in a Linked List.cpp
+++ leetcode questions/ds-algorithm/medium/Swapping Nodes in a Linked List.cpp
@@ -1,1 +1,32 @@
 //https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+
+class Solution {
+public:
+    ListNode* n1;
+    ListNode* n2;
+    int n;
+
+    ListNode* swapNodes(ListNode* head, int k) {
+      traverse(head, 0, k);
+      
+      int temp = n1->val;
+      n1->val = n2->val;
+      n2->val = temp;
+      return  head;
+    }
+    void traverse(ListNode* head, int current, int k) {
+      if(!head) {
+        n = current;
+        return;
+      }
+
+      if(current == k-1)
+        n1 = head;
+      
+      traverse(head->next, current + 1, k);
+    
+      if(current == n - k) 
+        n2 = head;
+
+    }
+};
