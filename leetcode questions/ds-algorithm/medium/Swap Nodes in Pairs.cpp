8396b7d579ba5e0e8ae21de9982704a434f6c26e
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-05-16 23:33:55 +0300

    Update Swap Nodes in Pairs.cpp

--- leetcode questions/ds-algorithm/medium/Swap Nodes in Pairs.cpp
+++ leetcode questions/ds-algorithm/medium/Swap Nodes in Pairs.cpp
@@ -1,1 +1,30 @@
+//https://leetcode.com/problems/swap-nodes-in-pairs/
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+      ListNode* nextStep = head;
+      ListNode* current;
+      ListNode* next;
+      if(!head || !head->next) return head;
+      head = head->next;
+      while(nextStep && nextStep->next) {
+        current = nextStep;
+        next = current->next;
+        nextStep = next->next;
+        current->next = (nextStep)? (nextStep->next)? nextStep->next: nextStep: nextStep;
+        next->next = current;
+      }
+      return head;
+     } 
+};
