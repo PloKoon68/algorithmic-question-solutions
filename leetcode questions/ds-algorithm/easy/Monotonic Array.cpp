b2d2aa3e3f52e99b9d9a964c88d56017cd303d84
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2023-09-29 21:47:42 +0300

    Update Monotonic Array.cpp

--- leetcode questions/ds-algorithm/easy/Monotonic Array.cpp
+++ leetcode questions/ds-algorithm/easy/Monotonic Array.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        if(nums.size() == 1)
+            return true;
+
+
+        int i = 1;
+        while(i < nums.size() && nums[i] == nums[i-1]) 
+            i++;
+        
+        if(i == nums.size())
+            return true;
+
+
+        //inc
+        if(nums[i] > nums[i - 1]) 
+            while(i < nums.size()) {
+                if(nums[i] < nums[i-1])
+                    return false;
+                i++;
+            }
+        else 
+            while(i < nums.size()) {
+                if(nums[i] > nums[i-1])
+                    return false;
+                i++;
+            }
+        
+        return true;
+
+    }
+};
+
+
 
