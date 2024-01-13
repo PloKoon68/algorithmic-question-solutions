714394c4d9863c900386ae0b00372292fc8e04db
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-01-13 13:47:06 +0300

    Update Majority Element.cpp

--- leetcode questions/ds-algorithm/easy/Majority Element.cpp
+++ leetcode questions/ds-algorithm/easy/Majority Element.cpp
@@ -1,2 +1,28 @@
 //https://leetcode.com/problems/majority-element/
 
+class Solution {
+public:
+    
+int majorityElement(vector<int>& nums) {
+    if (nums.size() == 1)
+        return nums[0];
+
+    int count = 1, d = nums[0], i = 1;
+    int h = nums.size() / 2;
+
+    while (i < nums.size() && count <= h) {
+        if (nums[i] == d)
+            count++;
+        else {
+            if (count > 0) 
+                count--;
+            else {
+                d = nums[i];
+                count++;
+            }
+        }
+        i++;
+    }
+    return d;
+}
+};
