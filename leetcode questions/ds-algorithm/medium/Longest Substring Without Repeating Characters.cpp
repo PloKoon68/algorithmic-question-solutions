f41095d5475a58508d73f7196ead96ea32f6622d
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-01-23 00:50:57 +0300

    Create Longest Substring Without Repeating Characters.cpp

---
+++ leetcode questions/ds-algorithm/medium/Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,55 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+class Solution {
+   
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0)
+            return 0;
+        if(s.length() == 1)
+            return 1;
+
+        int[] arr = new int[127];
+        int st = 0, q = 1;
+        int maxLen = 0, len = 0;
+
+        arr[s.charAt(st)] = 1;
+
+        while(q < s.length()) {
+            len = 0;
+            //if the remaining is already less then max len just pass
+            if(s.length() - st < maxLen)
+                break;
+
+            //find the repeating char
+            while(true) {
+                if(q == s.length())
+                    return (q - st > maxLen)? q - st: maxLen;
+                if(arr[s.charAt(q)] == 0)
+                    arr[s.charAt(q)] = ++q;
+                else {
+
+
+
+                    len = q - st;
+
+                    while(st != arr[s.charAt(q)] - 1) {
+                        arr[s.charAt(st)] = 0;
+                        st++;
+                    }
+                    st++;
+
+                    arr[s.charAt(q)] = ++q;
+                    break;
+                }
+            }
+
+            //set max len
+            maxLen = maxLen < len? len: maxLen;
+
+
+        }
+
+
+        return maxLen;
+    }
+}
