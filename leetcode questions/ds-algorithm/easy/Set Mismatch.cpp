828bf7a89c6055d6d14579d7b5110ede1299c39f
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-01-22 17:26:11 +0300

    Update Set Mismatch.cpp

--- leetcode questions/ds-algorithm/easy/Set Mismatch.cpp
+++ leetcode questions/ds-algorithm/easy/Set Mismatch.cpp
@@ -1,1 +1,27 @@
 //https://leetcode.com/problems/set-mismatch/description/
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> recorded(nums.size()+1, 0);
+
+        int missing, duplicated, sum = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int current = nums[i];
+
+            if(!recorded[current]) {
+                recorded[current]++;
+                sum += current;
+            }
+            else
+                duplicated = current;
+        }
+
+        int n = nums.size();
+        missing = n*(n+1)/2 - sum;
+        
+        vector<int> result = {duplicated, missing};
+        return result;
+    }
+};
