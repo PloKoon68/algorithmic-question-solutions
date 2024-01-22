df80993baed14acdf9fda1c80fb9c06aa2e742df
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-01-23 00:39:57 +0300

    Update Permutations.cpp

--- leetcode questions/ds-algorithm/medium/Permutations.cpp
+++ leetcode questions/ds-algorithm/medium/Permutations.cpp
@@ -1,1 +1,44 @@
  //https://leetcode.com/problems/permutations/description/
+
+
+class Solution {
+public:
+    //result array
+    vector<vector<int>> permutations;
+
+    //swap function  
+    void swap(vector<int>& arr, int ind1, int ind2) {
+        int temp = arr[ind1];
+        arr[ind1] = arr[ind2];
+        arr[ind2] = temp;
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        //start with level one, which is the initial version (input array nums)
+        permutation(nums, 0);
+        return permutations;
+    }
+
+    void permutation(vector<int> prev, int level) {
+        //if it came to the lest level, which is for the last index, just add the array obtained so far to the resultant array and return
+        if (level == prev.size() - 1) {
+            permutations.push_back(prev);
+            return;
+        }
+
+        
+        int i = level;
+        while (i < prev.size()) {
+            //each step copy the previous levels array 
+            vector<int> current = prev;
+        
+            //modify the previous arrays copy based on the current step
+            swap(current, level, i);
+            
+            //go to the next level (right next elements index) with modified (current) array
+            permutation(current, level+1);
+
+            i++;
+        }
+    }
+
+};
