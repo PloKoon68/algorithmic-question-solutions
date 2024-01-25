0e46dbf952daeba4f2196db1884c9f835a4e3b55
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-01-25 03:37:42 +0300

    Update Pseudo-Palindromic Paths in a Binary Tree.cpp

--- leetcode questions/ds-algorithm/medium/Pseudo-Palindromic Paths in a Binary Tree.cpp
+++ leetcode questions/ds-algorithm/medium/Pseudo-Palindromic Paths in a Binary Tree.cpp
@@ -1,1 +1,46 @@
 //https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/description/?envType=daily-question&envId=2024-01-24
+
+
+class Solution {
+public:
+    unordered_set<int> oddNums;
+    int count = 0;
+    int pseudoPalindromicPaths(TreeNode* root) {
+        process(root);
+        return count;
+    }
+    void process(TreeNode* currentNode) {
+        //if node is null just return
+        if (!currentNode)
+            return;
+
+
+        auto it = oddNums.find(currentNode->val);
+        bool found = it != oddNums.end();
+        // Element found, remove it from set
+        if (found) 
+            oddNums.erase(it);
+        // Element not found, add it to the set
+        else
+            oddNums.insert(currentNode->val);
+
+
+        //if at the final node of the path, check if it is palindrome and return
+        if (!currentNode->left && !currentNode->right) {
+            if (oddNums.size() <= 1)
+                count++;
+        }
+        else {
+            process(currentNode->left);
+            process(currentNode->right);
+        }
+
+        //reverse the process to undo it while going back upward
+        if (!found) {
+            it = oddNums.find(currentNode->val);
+            oddNums.erase(it);
+        }
+        else
+            oddNums.insert(currentNode->val);
+    }
+};
