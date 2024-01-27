dfd9230b0fda3b6252ff41fcf0538800d940dd68
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-01-28 01:32:08 +0300

    Update Reverse Words in a String.cpp

--- leetcode questions/ds-algorithm/medium/Reverse Words in a String.cpp
+++ leetcode questions/ds-algorithm/medium/Reverse Words in a String.cpp
@@ -1,1 +1,38 @@
 //https://leetcode.com/problems/reverse-words-in-a-string/description/
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string result = "";
+
+        //for first word, do manually without any space
+        int i = 0;
+        string word = "";
+        while(i < s.size() && s[i] == ' ')
+            i++;
+        while(i < s.size() && s[i] != ' ')
+            word += s[i++];c
+
+        while(i < s.size() && s[i] == ' ')
+            i++;
+        
+        result = word;        
+        if(i == s.size())
+                return result;
+
+        while(true) {
+            string word = "";
+            while(i < s.size() && s[i] != ' ')
+                word += s[i++];
+
+            while(i < s.size() && s[i] == ' ')
+                i++;
+            
+            result = word + " " + result;
+
+            if(i == s.size())
+                return result;
+        }
+    }
+};
