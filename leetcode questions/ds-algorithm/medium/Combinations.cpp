5c494fe4e7c8b31a8d1f49693e2f0677d00ee2c7
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-01-28 09:14:51 +0300

    Update Combinations.cpp

--- leetcode questions/ds-algorithm/medium/Combinations.cpp
+++ leetcode questions/ds-algorithm/medium/Combinations.cpp
@@ -1,1 +1,24 @@
 //https://leetcode.com/problems/combinations/description/
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> combinations;
+        vector<int> combination(k, 0);
+        process(combinations, combination, &n, &k, 0, 0);
+
+    return combinations;    
+    }
+
+    void process(vector<vector<int>> &combinations, vector<int>& combination, int* n, int* k, int level, int prev) {
+        if(level == *k) {
+            combinations.push_back(combination);
+            return;
+        }
+
+        for(int i = prev + 1; i <= *n - (*k - level - 1); i++) {
+            combination[level] = i;
+            process(combinations, combination, n, k, level + 1, i);
+        }
+    }
+};
