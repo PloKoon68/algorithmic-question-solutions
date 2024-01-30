1694d2106e1fc068250fbe990c3eba15a2befab2
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-01-30 23:43:40 +0300

    Update Evaluate Reverse Polish Notation.cpp

--- leetcode questions/ds-algorithm/medium/Evaluate Reverse Polish Notation.cpp
+++ leetcode questions/ds-algorithm/medium/Evaluate Reverse Polish Notation.cpp
@@ -1,1 +1,39 @@
 //https://leetcode.com/problems/evaluate-reverse-polish-notation/description/?envType=daily-question&envId=2024-01-30
+
+class Solution {
+public:
+    int i = 0;
+    unordered_set<string> operands = {"+", "-", "*", "/"};
+
+    int evalRPN(vector<string>& tokens) {
+        i = tokens.size() - 1;
+        return process(tokens);  
+    }
+
+    int process(vector<string>& tokens) {
+        auto it = operands.find(tokens[i]);
+        int right, left;
+        string operand;
+        //is operand
+        if(it != operands.end()) {
+            operand = tokens[i--];
+            right = process(tokens);
+        }
+        else 
+            return stoi(tokens[i--]);
+        
+        left = process(tokens);
+
+        if (operand == "+") 
+            return left + right;
+        else if (operand == "-") 
+            return left - right;
+        else if (operand == "*") 
+            return left * right;
+        else if (operand == "/") 
+            return left / right;
+
+        return 0;  
+    }
+    
+};
