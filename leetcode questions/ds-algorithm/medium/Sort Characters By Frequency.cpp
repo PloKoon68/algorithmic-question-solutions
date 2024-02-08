c43d48277559bf1e50e6bfe9f22265aa79e4b27e
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-02-08 16:39:22 +0300

    Update Sort Characters By Frequency.cpp

--- leetcode questions/ds-algorithm/medium/Sort Characters By Frequency.cpp
+++ leetcode questions/ds-algorithm/medium/Sort Characters By Frequency.cpp
@@ -3,30 +3,28 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        auto cmp = [](const pair<char, int>& a, const pair<char, int>& b) {
-            return a.second < b.second;
-        };
-        
+        auto cmp = [](const pair<char, int>& a, const pair<char, int>& b) {return a.second < b.second;};
         priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(cmp)> pq(cmp);
-        
-        unordered_map<char, int> hm;
-        
-        for (char c : s) {
-            hm[c]++;
+        int freq[62] = {0};
+        for (char ch : s) {
+            if (ch >= 'a') freq[ch - 'a']++;
+            else if (ch >= 'A') freq[ch - 'A' + 26]++;
+            else freq[ch - '0' + 52]++;
         }
-        
-        for (const auto& entry : hm) {
-            pq.push(make_pair(entry.first, entry.second));
-        }
-        
+        for (int i = 0; i < 26; i++)
+            if (freq[i]) pq.push(make_pair((char)'a' + i, freq[i]));
+        //for uppercase chars
+        for (int i = 26; i < 52; i++)
+            if (freq[i]) pq.push(make_pair((char)'A' + i - 26, freq[i]));
+        for (int i = 52; i < 62; i++)
+            if (freq[i]) pq.push(make_pair((char)'0' + i - 52, freq[i]));
+
         string result = "";
         while (!pq.empty()) {
-            pair<char, int> p = pq.top();
+            result.append(pq.top().second, pq.top().first);
             pq.pop();
-            result.append(p.second, p.first);
         }
         
         return result;
     }
 };
-
