f938baf930417d2a1b4e4a8f68692b80e86bff59
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-02-17 00:52:51 +0300

    Update Least Number of Unique Integers after K Removals.cpp

--- leetcode questions/ds-algorithm/medium/Least Number of Unique Integers after K Removals.cpp
+++ leetcode questions/ds-algorithm/medium/Least Number of Unique Integers after K Removals.cpp
@@ -1,1 +1,22 @@
 //https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/description/?envType=daily-question&envId=2024-02-16
+
+
+
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int, int> counts;
+        for (int i : arr) counts[i]++;
+        
+        auto cmp = [](int a, int b) { return b < a; };
+        priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
+        for (const auto& pair : counts) pq.push(pair.second);
+
+        while (!pq.empty()) {
+            if (k < pq.top()) return pq.size();
+            k -= pq.top();
+            pq.pop();
+        }
+        return 0;
+    }
+};
