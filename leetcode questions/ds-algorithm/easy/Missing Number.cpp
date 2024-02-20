e025ca47d2565801f3e7abe3932508cad15c803a
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-02-20 23:24:39 +0300

    Create Missing Number.cpp

---
+++ leetcode questions/ds-algorithm/easy/Missing Number.cpp
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/missing-number/description/?envType=daily-question&envId=2024-02-20
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0, n = nums.size();
+        for (int num : nums) sum += num;
+        return n * (n + 1) / 2 - sum;
+    }
+};
