9e173e4b4a84076ca6d9882ad2a92c520d8d1d26
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-03-09 13:04:37 +0300

    Create Minimum Common Value.cpp

---
+++ leetcode questions/ds-algorithm/easy/Minimum Common Value.cpp
@@ -0,0 +1,2 @@
+//question:      https://leetcode.com/problems/minimum-common-value/description/
+//my solution:   https://leetcode.com/problems/minimum-common-value/solutions/4847536/beats-95-98-c-two-pointer/
