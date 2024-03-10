cf89246924b55a302c454b29362d60c0742306d2
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-03-10 12:03:11 +0300

    Update Intersection of Two Arrays.cpp

--- leetcode questions/ds-algorithm/easy/Intersection of Two Arrays.cpp
+++ leetcode questions/ds-algorithm/easy/Intersection of Two Arrays.cpp
@@ -1,1 +1,16 @@
+//https://leetcode.com/problems/intersection-of-two-arrays/description/?envType=daily-question&envId=2024-03-10
 
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> added;
+        for(int num: nums1) added.insert(num);
+        vector<int> result;
+        for(int num: nums2) 
+            if(added.contains(num)) {
+                result.push_back(num);
+                added.erase(num);
+            }
+        return result;
+    }
+};
