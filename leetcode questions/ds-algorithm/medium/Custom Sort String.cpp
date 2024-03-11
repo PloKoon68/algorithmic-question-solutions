946d17af8af76c8b218b26a0a2429b2ec8540351
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-03-11 21:31:37 +0300

    Update Custom Sort String.cpp

--- leetcode questions/ds-algorithm/medium/Custom Sort String.cpp
+++ leetcode questions/ds-algorithm/medium/Custom Sort String.cpp
@@ -1,2 +1,29 @@
 //question:       https://leetcode.com/problems/custom-sort-string/description/
 //my solution:    https://leetcode.com/problems/custom-sort-string/solutions/4861280/beats-100-c-priority-queue/
+
+
+
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        unordered_map<char, int> alphabet;
+        
+        for (int i = 0; i < order.size(); i++) alphabet[order[i]] = i;
+
+        auto cmp = [](const pair<char, int>& a, const pair<char, int>& b) {
+            return a.second > b.second; // Sort based on the second element
+            };
+        priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(cmp)> pq(cmp);
+        
+        for (char ch : s) pq.push(make_pair(ch, alphabet[ch]));
+
+        string result = "";
+        while (!pq.empty()) {
+            result += pq.top().first;
+            pq.pop();
+        }
+
+        return result;
+    }
+};
