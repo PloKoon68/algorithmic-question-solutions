f17bb8af7d15653291e0f0b36c7306db1a23c87e
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-03-11 21:31:27 +0300

    Update Custom Sort String.cpp

--- leetcode questions/ds-algorithm/medium/Custom Sort String.cpp
+++ leetcode questions/ds-algorithm/medium/Custom Sort String.cpp
@@ -1,1 +1,2 @@
-
+//question:       https://leetcode.com/problems/custom-sort-string/description/
+//my solution:    https://leetcode.com/problems/custom-sort-string/solutions/4861280/beats-100-c-priority-queue/
