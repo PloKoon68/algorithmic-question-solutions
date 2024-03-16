db53d65e1bc6201fdbf5fd9f833db7065f005257
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-03-17 01:37:13 +0300

    Update Contiguous Array.cpp

--- leetcode questions/ds-algorithm/medium/Contiguous Array.cpp
+++ leetcode questions/ds-algorithm/medium/Contiguous Array.cpp
@@ -1,1 +1,23 @@
 //https://leetcode.com/problems/contiguous-array/description/
+
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+
+        int sum = 0, maxDist = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i] ? 1 : -1;
+
+            if (mp.find(sum) != mp.end()) {
+                int distance = i - mp[sum];
+                if (distance > maxDist) maxDist = distance;
+            }
+            else mp[sum] = i;
+        }
+
+        return maxDist;
+    }
+};
