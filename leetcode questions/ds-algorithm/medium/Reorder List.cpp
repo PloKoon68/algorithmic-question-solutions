905e0c33390abac0556d87c7b0600a6df798dddc
Mehmet ÖRS <75950443+PloKoon68@users.noreply.github.com>
2024-03-23 23:32:28 +0300

    Update Reorder List.cpp

--- leetcode questions/ds-algorithm/medium/Reorder List.cpp
+++ leetcode questions/ds-algorithm/medium/Reorder List.cpp
@@ -1,2 +1,43 @@
 //question:      https://leetcode.com/problems/reorder-list/description/
 //my solution:   https://leetcode.com/problems/reorder-list/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(!head || !head->next) return;
+        stack<ListNode*> st;
+        ListNode* first = head;
+        ListNode* second = head->next;
+        while(second && second->next) {
+            first = first->next;
+            second = second->next->next;
+        }
+        first = first->next;
+        while(first) {
+            st.push(first);
+            first = first->next;
+        }
+
+        first = head;
+        second;
+        while(!st.empty()) {
+            second = first->next;
+            first->next = st.top();
+            st.top()->next = second;
+            st.pop();
+            first = second;
+        }
+        first->next = nullptr;
+    }
+};
